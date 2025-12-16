0c9886dc9cdc496f3e7df84a661dd8b5111b000a
Esrevi Huynh <esrevi.huynh@filigran.io>
2025-12-16 15:13:29 +0100

    feat: add GithubService to fetch PR's comments, reviews

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/*.test.ts', '**/*.spec.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: false,
+    }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*.spec.ts',
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  // Mock octokit to avoid ESM issues in Jest
+  moduleNameMapper: {
+    '^octokit$': '<rootDir>/src/__mocks__/octokit.mock.ts',
+  },
+};
