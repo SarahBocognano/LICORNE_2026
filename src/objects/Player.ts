270de45d9f3a99eb43fbdd14e25865904aa19c1b
Esrevi Huynh <esrevi.huynh@filigran.io>
2025-12-17 14:08:28 +0100

    feat: add character animation

--- src/objects/Player.ts
+++ src/objects/Player.ts
@@ -1,42 +1,88 @@
-import Phaser from 'phaser';
+import { Input, Scene } from 'phaser';
 
-export class Player extends Phaser.Physics.Arcade.Sprite {
-  private speed = 200;
+import { EVENTS_NAME, GameStatus } from '../consts';
+import { Actor } from './Actor';
 
-  constructor(scene: Phaser.Scene, x: number, y: number) {
+export class Player extends Actor {
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private keySpace!: Input.Keyboard.Key;
 
-    super(scene, x, y, 'pixel');
+  constructor(scene: Scene, x: number, y: number) {
+    super(scene, x, y, 'a-king');
+    this.setFrame('run-0');
 
-    if (!scene.textures.exists('pixel')) {
-      const graphics = scene.make.graphics();
-      graphics.fillStyle(0xffffff);
-      graphics.fillRect(0, 0, 1, 1);
-      graphics.generateTexture('pixel', 1, 1);
-      graphics.destroy();
-    }
+    const keyboard = this.scene.input.keyboard;
+    if (!keyboard) return;
 
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
+    this.cursors = keyboard.createCursorKeys();
 
-    this.setCollideWorldBounds(true);
-    this.setTint(0xff00ff);
-    this.setDisplaySize(32, 32);
+    this.keySpace = keyboard.addKey(32);
+    this.keySpace.on('down', (event: KeyboardEvent) => {
+      this.anims.play('attack', true);
+      this.scene.game.events.emit(EVENTS_NAME.attack);
+    });
+
+    // PHYSICS
+    this.getBody().setSize(30, 30);
+    this.getBody().setOffset(8, 0);
+
+    // ANIMATIONS
+    this.initAnimations();
+
+    this.on('destroy', () => {
+      this.keySpace.removeAllListeners();
+    });
   }
 
-  update(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
-    // set movement of the characters with keyboard
-    this.setVelocity(0);
+  update(): void {
+    this.getBody().setVelocity(0);
 
-    if (cursors.left.isDown) {
-      this.setVelocityX(-this.speed);
-    } else if (cursors.right.isDown) {
-      this.setVelocityX(this.speed);
+    if (!this.body) {
+      return
     }
 
-    if (cursors.up.isDown) {
-      this.setVelocityY(-this.speed);
-    } else if (cursors.down.isDown) {
-      this.setVelocityY(this.speed);
+    if (this.cursors.up?.isDown) {
+      this.body.velocity.y = -110;
+      !this.anims.isPlaying && this.anims.play('run', true);
     }
+
+    if (this.cursors.left?.isDown) {
+      this.body.velocity.x = -110;
+      this.checkFlip();
+      this.getBody().setOffset(48, 15);
+      !this.anims.isPlaying && this.anims.play('run', true);
+    }
+
+    if (this.cursors.down?.isDown) {
+      this.body.velocity.y = 110;
+      !this.anims.isPlaying && this.anims.play('run', true);
+    }
+
+    if (this.cursors.right?.isDown) {
+      this.body.velocity.x = 110;
+      this.checkFlip();
+      this.getBody().setOffset(15, 15);
+      !this.anims.isPlaying && this.anims.play('run', true);
+    }
+  }
+
+  private initAnimations(): void {
+    this.scene.anims.create({
+      key: 'run',
+      frames: this.scene.anims.generateFrameNames('a-king', {
+        prefix: 'run-',
+        end: 7,
+      }),
+      frameRate: 8,
+    });
+
+    this.scene.anims.create({
+      key: 'attack',
+      frames: this.scene.anims.generateFrameNames('a-king', {
+        prefix: 'attack-',
+        end: 2,
+      }),
+      frameRate: 8,
+    });
   }
 }
